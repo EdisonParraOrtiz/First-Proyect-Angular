f9ea6e8ec14a2eb419bcf803e1d0311319d8c8f1
Your Name <you@example.com>
2022-11-18 15:56:34 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'store';
+  title = 'Hola Edison';
+
+  getName(): void {
+    
+    console.log('Hola Edison')
+  }
 }
